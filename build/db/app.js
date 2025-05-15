1eb117e546e091bbc450c6b1f2c2a8df56191bfe
scottgirling <scottgirling0812@gmail.com>
2025-05-15 15:45:04 +0100

    setup doc structure, npm installs, ts config and package.json

---
+++ build/db/app.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+app.use(express.json());
+module.exports = app;
