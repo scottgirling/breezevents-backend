25a0b7906e88a776b266afe4a87ba36fff0e65b6
scottgirling <scottgirling0812@gmail.com>
2025-05-20 16:39:59 +0100

    add getEvents to app.ts

--- db/app.ts
+++ db/app.ts
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 const getEndpoints = require ("../controllers/getEndpoints");
+const getEvents = require("../controllers/getEvents");
 
 app.use(express.json());
 
 app.get("/api", getEndpoints);
+
+app.get("/api/events", getEvents);
 
 module.exports = app;
