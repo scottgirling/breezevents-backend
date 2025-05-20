640390fd248d0118a936560bebbd66411370f07b
scottgirling <scottgirling0812@gmail.com>
2025-05-20 16:38:55 +0100

    add tests for GET /api/events endpoint

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,3 +1,8 @@
+import { CustomResponse } from "../controllers/interfaces/types";
+import { Event } from "../db/data/test-data/events";
+import {expect, jest, test} from '@jest/globals';
+import 'jest-extended';
+
 const request = require("supertest");
 const app = require("../db/app");
 const connection = require("../db/connection");
@@ -18,8 +23,43 @@ describe("GET /api", () => {
         return request(app)
         .get("/api")
         .expect(200)
-        .then(({ body: { endpoints }} : { body: any }) => {
+        .then(({ body: { endpoints } } : { body: CustomResponse }) => {
             expect(endpoints).toEqual(endpointsJSON);
+        });
+    });
+});
+
+describe("GET /api/events", () => {
+    test("200: responds with an array of event objects, with the appropriate properties and status code", () => {
+        return request(app)
+        .get("/api/events")
+        .expect(200)
+        .then(({ body: { events } } : { body: CustomResponse }) => {
+            expect(Array.isArray(events)).toBe(true);
+            expect(events.length).toBe(5);
+            events.forEach((event) => {
+                expect(event).toHaveProperty("event_id", expect.any(Number));
+                expect(event).toHaveProperty("title", expect.any(String));
+                expect(event).toHaveProperty("slug", expect.any(String));
+                expect(event).toHaveProperty("event_overview", expect.any(String));
+                expect(event).toHaveProperty("description", expect.any(String));
+                expect(event).toHaveProperty("start_time", expect.any(String));
+                expect(event).toHaveProperty("end_time", expect.any(String));
+                expect(event).toHaveProperty("timezone", expect.any(String));
+                expect(event).toHaveProperty("venue_id", expect.any(Number));
+                expect(event).toHaveProperty("is_online", expect.any(Boolean));
+                expect(event).toHaveProperty("host_id", expect.any(Number));
+                expect(event).toHaveProperty("event_type", expect.any(String));
+                expect(event).toHaveProperty("capacity", expect.any(Number));
+                expect(event).toHaveProperty("attendees_count", expect.any(Number));
+                expect(event).toHaveProperty("is_free", expect.any(Boolean));
+                expect(event).toHaveProperty("price", expect.any(Number));
+                expect(event).toHaveProperty("event_image_url", expect.any(String));
+                expect(event).toHaveProperty("is_published", expect.any(Boolean));
+                expect(event).toHaveProperty("created_at", expect.any(String));
+                expect(event).toHaveProperty("last_updated_at", expect.any(String));
+                expect(Object.entries(event).length).toBe(20);
+            });
         });
     });
 });
