b1eedb1a41bd85a093450bb3bd7240b58aade963
scottgirling <scottgirling0812@gmail.com>
2025-05-23 12:07:34 +0100

    add new 404 error logic for POST /api/user_event endpoint

--- models/addUserEvent.ts
+++ models/addUserEvent.ts
@@ -1,7 +1,6 @@
 const db = require("../db/connection");
 
 const addUserEvent = (user_id: number, event_id: number) => {
-    console.log(user_id, event_id)
     return db.query("INSERT INTO user_events (user_id, event_id) VALUES ($1, $2) RETURNING *", [user_id, event_id])
     .then(({ rows } : { rows: Array<object> }) => {
         return rows[0];
