b0b857d996843a68860d3ad5bf9d091a93c64eb7
scottgirling <scottgirling0812@gmail.com>
2025-05-26 17:52:09 +0100

    remove comments

--- db/data/development-data/user_events.ts
+++ db/data/development-data/user_events.ts
@@ -2,24 +2,24 @@ import { UserEvent } from "../test-data/user_events";
 
 export const userEvents: UserEvent[] = [
 	{
-		"user_id": 1,   // Alice Thompson (attendee)
-		"event_id": 1   // UK Tech Expo 2025 (attending)
+		"user_id": 1,
+		"event_id": 1
 	},
 	{
-		"user_id": 1,   // Alice Thompson (attendee)
-		"event_id": 2   // Green Future Forum (hosting)
+		"user_id": 1,
+		"event_id": 2
 	},
 	{
-		"user_id": 1,   // Alice Thompson (attendee)
-		"event_id": 5   // Women in Tech Leadership Forum (attending)
+		"user_id": 1,
+		"event_id": 5
 	},
 	{
-		"user_id": 3,   // Daniel O'Brien (attendee)
-		"event_id": 1   // UK Tech Expo 2025 (attending)
+		"user_id": 3,
+		"event_id": 1
 	},
 	{
-		"user_id": 3,   // Daniel O'Brien (host)
-		"event_id": 2   // Green Future Forum (hosting)
+		"user_id": 3,
+		"event_id": 2
 	},
 	{
 		"user_id": 5,
