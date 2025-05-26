85256f3107e181024e0ce061f3ea5eaa72a4a900
scottgirling <scottgirling0812@gmail.com>
2025-05-26 13:35:02 +0100

    refactor util to take 'user_id' rather than 'username'

--- models/utils/checkUserExists.ts
+++ models/utils/checkUserExists.ts
@@ -1,7 +1,7 @@
 const db = require("../../db/connection");
 
-const checkUserExists = (username: string) => {
-    return db.query("SELECT * FROM users WHERE username = $1", [username])
+const checkUserExists = (user_id: string) => {
+    return db.query("SELECT * FROM users WHERE user_id = $1", [user_id])
     .then(({ rows } : { rows: Array<object> }) => {
         if (!rows.length) {
             return Promise.reject({ status: 404, msg: "Profile not found." });
