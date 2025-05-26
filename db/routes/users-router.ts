50f78b9bbbfd4e76d61356dab812662404b1e90e
scottgirling <scottgirling0812@gmail.com>
2025-05-26 12:20:33 +0100

    add POST /api/users controller to users-router

--- db/routes/users-router.ts
+++ db/routes/users-router.ts
@@ -1,7 +1,12 @@
 const getUserByUsername = require("../../controllers/getUserByUsername");
 const getEventsByUsername = require("../../controllers/getEventsByUsername");
+const postUser = require("../../controllers/postUser");
 
 const usersRouter = require("express").Router();
+
+usersRouter
+    .route("/")
+    .post(postUser)
 
 usersRouter
     .route("/:username")
