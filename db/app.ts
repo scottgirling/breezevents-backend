b2bc111ca3b7428e5d96d6e430b456f75e14b8ca
scottgirling <scottgirling0812@gmail.com>
2025-05-28 14:47:00 +0100

    enable cors

--- db/app.ts
+++ db/app.ts
@@ -4,8 +4,10 @@ import { CustomRequest, CustomResponse, CustomError } from "../controllers/inter
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
 const apiRouter = require("./routes/api-router");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
