c0db5452fff28219b967dc510a983f953781f0d8
scottgirling <scottgirling0812@gmail.com>
2025-06-04 22:07:03 +0100

    move postCheckoutSession into own controller file

---
+++ controllers/postCheckoutSession.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Response } from "express";
+import { CustomRequest } from "./interfaces/types";
+
+require("dotenv").config({
+    path: `${__dirname}/../../.env.STRIPE_SECRET_KEY`
+});
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+
+const postCheckoutSession = async (request: CustomRequest, response: Response, next: NextFunction) => {
+    const { event, ticketQuantity } = request.body;
+
+    stripe.checkout.sessions.create({
+        line_items: [
+            {
+                price_data: {
+                    currency: "gbp",
+                    product_data: {
+                        name: event.title
+                    },
+                    unit_amount: Math.round(event.price * 100)
+                },
+                quantity: ticketQuantity
+            }
+        ],
+        mode: "payment",
+        success_url: "http://localhost:5173/success",
+        cancel_url: "http://localhost:5173/events"
+    })
+    .then((session: any) => {
+        response.json({ url: session.url });
+    })
+    .catch((error: Error) => {
+        next(error);
+    });
+}
+
+module.exports = postCheckoutSession;
