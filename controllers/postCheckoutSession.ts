dc53b88bf88ca876a868833ea4dd89e15f42c841
scottgirling <scottgirling0812@gmail.com>
2025-06-05 13:19:26 +0100

    add metadata to stripe.checkout.sessions

--- controllers/postCheckoutSession.ts
+++ controllers/postCheckoutSession.ts
@@ -2,13 +2,16 @@ import { NextFunction, Response } from "express";
 import { CustomRequest } from "./interfaces/types";
 
 require("dotenv").config({
-    path: `${__dirname}/../../.env.STRIPE_SECRET_KEY`
+    path: `${__dirname}/../../.env.STRIPE_SECRETS`
 });
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+if (!stripe) throw new Error("STRIPE_SECRET_KEY is missing.");
 
-const postCheckoutSession = async (request: CustomRequest, response: Response, next: NextFunction) => {
+
+
+const postCheckoutSession = (request: CustomRequest, response: Response, next: NextFunction) => {
     const { event, ticketQuantity } = request.body;
 
     stripe.checkout.sessions.create({
@@ -24,6 +27,10 @@ const postCheckoutSession = async (request: CustomRequest, response: Response, n
                 quantity: ticketQuantity
             }
         ],
+        metadata: {
+            event,
+            ticketQuantity
+        },
         mode: "payment",
         success_url: "http://localhost:5173/success",
         cancel_url: "http://localhost:5173/events"
