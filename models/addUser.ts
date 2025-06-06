8ec41104290230d872846073dec01d0027b67ffc
scottgirling <scottgirling0812@gmail.com>
2025-06-06 21:02:22 +0100

    refactor models to fit new 'user_id'

--- models/addUser.ts
+++ models/addUser.ts
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 
-const addUser = (name: string, username: string, email: string, password_hash: string, bio: string, avatar_url: string, role: string) => {
-    return db.query("INSERT INTO users (name, username, email, password_hash, bio, avatar_url, role) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", [name, username, email, password_hash, bio, avatar_url, role])
+const addUser = (user_id: string, name: string, username: string, email: string, bio: string, avatar_url: string, role: string) => {
+    return db.query("INSERT INTO users (user_id, name, username, email, bio, avatar_url, role) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *", [user_id, name, username, email, bio, avatar_url, role])
     .then(({ rows } : { rows: Array<object> }) => {
         return rows[0];
     })
--- models/updateUserById.ts
+++ models/updateUserById.ts
@@ -1,7 +1,7 @@
 const db = require("../db/connection");
 const checkUserExists = require("./utils/checkUserExists");
 
-const updateUserById = (user_id: number, name: string, username: string, email: string, password_hash: string, bio: string, avatar_url: string) => {
+const updateUserById = (user_id: number, name: string, username: string, email: string, bio: string, avatar_url: string) => {
     return checkUserExists(user_id)
     .then(() => {
         let queryValues = [];
@@ -21,11 +21,6 @@ const updateUserById = (user_id: number, name: string, username: string, email: 
         if (email) {
             queryValues.push(email);
             sqlQuery += `, email = $${queryValues.length}`;
-        }
-    
-        if (password_hash) {
-            queryValues.push(password_hash);
-            sqlQuery += `, password_hash = $${queryValues.length}`;
         }
     
         if (bio) {
