b111ee7c2549759dc693d76d80ad95ea00086e76
scottgirling <scottgirling0812@gmail.com>
2025-06-08 16:48:18 +0100

    add POST /api/event_tags to router

--- db/routes/api-router.ts
+++ db/routes/api-router.ts
@@ -2,6 +2,7 @@ const getEndpoints = require("../../controllers/getEndpoints");
 
 const apiRouter = require("express").Router();
 const eventsRouter = require("./events-router");
+const eventTagsRouter = require("./event_tags-router");
 const tagsRouter = require("./tags-router");
 const userEventsRouter = require("./user_events-router");
 const usersRouter = require("./users-router");
@@ -11,6 +12,7 @@ const sessionRouter = require("./session-router");
 const authRouter = require("./auth-router");
 
 apiRouter.use("/events", eventsRouter);
+apiRouter.use("/event_tags", eventTagsRouter);
 apiRouter.use("/tags", tagsRouter);
 apiRouter.use("/user_events", userEventsRouter);
 apiRouter.use("/users", usersRouter);
