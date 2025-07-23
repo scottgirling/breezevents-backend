9b615b4788b52b9665e63672336ece68e029f367
scottgirling <scottgirling0812@gmail.com>
2025-07-23 18:21:01 +0100

    amend createClient to test RLS

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -6,27 +6,28 @@ require("dotenv").config({
 });
 
 export const createClient = (context: SupasbaseContext) => {
-  return createServerClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, {
-    cookies: {
-      getAll() {
-        return parseCookieHeader(context.request.headers.cookie ?? '');
-      },
-      setAll(cookiesToSet: any) {
-        cookiesToSet.forEach(({ name, value, options } : { name: unknown, value: unknown, options: unknown}) => {
-          const serialized = serializeCookieHeader(name, value, options);
+  // return createServerClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, {
+  //   cookies: {
+  //     getAll() {
+  //       return parseCookieHeader(context.request.headers.cookie ?? '');
+  //     },
+  //     setAll(cookiesToSet: any) {
+  //       cookiesToSet.forEach(({ name, value, options } : { name: unknown, value: unknown, options: unknown}) => {
+  //         const serialized = serializeCookieHeader(name, value, options);
 
-          const existing = context.response.getHeader('Set-Cookie');
-          const newValue = existing
-            ? Array.isArray(existing)
-              ? [...existing, serialized]
-              : [existing, serialized]
-            : serialized;
+  //         const existing = context.response.getHeader('Set-Cookie');
+  //         const newValue = existing
+  //           ? Array.isArray(existing)
+  //             ? [...existing, serialized]
+  //             : [existing, serialized]
+  //           : serialized;
 
-          context.response.setHeader('Set-Cookie', newValue);
-        });
-      },
-    },
-  });
+  //         context.response.setHeader('Set-Cookie', newValue);
+  //       });
+  //     },
+  //   },
+  // });
+  return createServerClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
 };
 
 module.exports = createClient;
