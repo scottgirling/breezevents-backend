b45678dc968ea8c124edd9f50bb5f4e71bc8679b
scottgirling <scottgirling0812@gmail.com>
2025-07-26 17:44:05 +0100

    add logs identify issue

--- controllers/getAuthConfirmation.ts
+++ controllers/getAuthConfirmation.ts
@@ -3,16 +3,20 @@ import { createClient } from "../lib/supabaseClient";
 
 export const getAuthConfirmation = async (request: Request, response: Response) => {
 	const token_hash = request.query.token_hash;
+	console.log(token_hash, "<--- token_hash")
 	const type = request.query.type;
+	console.log(type, "<--- type (before hard code)")
 	const next = typeof request.query.next === "string" ? request.query.next : "/";
-
+	
 	if (token_hash && type) {
 		try {
 			const supabase = createClient({ request, response });
-			const { error } = await supabase.auth.verifyOtp({
-				type,
-				token_hash,
+			const { error, data } = await supabase.auth.verifyOtp({
+				type: "signup",
+				token_hash: token_hash as string,
 			});
+
+			console.log("OTP verification data: ", data)
 	
 			if (!error) {
 				return response.redirect(303, next);
