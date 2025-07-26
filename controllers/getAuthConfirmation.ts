71ee4f848f486f92d0c22ae37a14379856821d1d
scottgirling <scottgirling0812@gmail.com>
2025-07-26 18:10:19 +0100

    amend if condition for user auth confirmation

--- controllers/getAuthConfirmation.ts
+++ controllers/getAuthConfirmation.ts
@@ -3,22 +3,20 @@ import { createClient } from "../lib/supabaseClient";
 
 export const getAuthConfirmation = async (request: Request, response: Response) => {
 	const token_hash = request.query.token_hash;
-	console.log(token_hash, "<--- token_hash")
 	const type = request.query.type;
-	console.log(type, "<--- type (before hard code)")
 	const next = typeof request.query.next === "string" ? request.query.next : "/";
 	
 	if (token_hash && type) {
 		try {
 			const supabase = createClient({ request, response });
 			const { error, data } = await supabase.auth.verifyOtp({
-				type: "signup",
-				token_hash: token_hash as string,
+				type,
+				token_hash,
 			});
 
 			console.log("OTP verification data: ", data)
 	
-			if (!error) {
+			if (!error && data?.user) {
 				return response.redirect(303, next);
 			} else {
 				console.error("Error verifying OTP: ", error);
