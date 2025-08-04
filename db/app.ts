3e8fa7b5d698d67b9f654d46795467b5faa1b524
scottgirling <scottgirling0812@gmail.com>
2025-08-04 19:40:42 +0100

    refactor to use express req and res

--- db/app.ts
+++ db/app.ts
@@ -1,5 +1,5 @@
-import { NextFunction } from "express";
-import { CustomRequest, CustomResponse, CustomError } from "../controllers/interfaces/types";
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../controllers/interfaces/types";
 import { getAuthConfirmation } from "../controllers/getAuthConfirmation";
 import { handleOAuthSignIn } from "../controllers/handleOAuthSignIn";
 
@@ -18,28 +18,28 @@ app.get("/auth/confirm", getAuthConfirmation);
 app.get("/auth/callback", handleOAuthSignIn);
 app.use("/webhook", webhookRouter);
 
-app.use((error: CustomError, request: CustomRequest, response: CustomResponse, next: NextFunction) => {
+app.use((error: CustomError, request: Request, response: Response, next: NextFunction) => {
     if (error.status && error.msg) {
         response.status(error.status).send({ msg: error.msg });
     }
     next(error);
 });
 
-app.use((error: CustomError, request: CustomRequest, response: CustomResponse, next: NextFunction) => {
+app.use((error: CustomError, request: Request, response: Response, next: NextFunction) => {
     if (error.code === "22P02" || error.code === "42703") {
         response.status(400).send({ msg: "Invalid data type." });
     }
     next(error);
 });
 
-app.use((error: CustomError, request: CustomRequest, response: CustomResponse, next: NextFunction) => {
+app.use((error: CustomError, request: Request, response: Response, next: NextFunction) => {
     if (error.code === "23502") {
         response.status(400).send({ msg: "Invalid request - missing field(s)." });
     }
     next(error);
 });
 
-app.use((error: CustomError, request: CustomRequest, response:CustomResponse, next: NextFunction) => {
+app.use((error: CustomError, request: Request, response: Response, next: NextFunction) => {
     if (error.code === "23503") {
         response.status(404).send({ msg: "Invalid request - one or more ID not found." });
     }
