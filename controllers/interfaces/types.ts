703577052ce082cb91cb9e1b8b7084623479046a
scottgirling <scottgirling0812@gmail.com>
2025-08-05 13:10:58 +0100

    add/remove types

--- controllers/interfaces/types.ts
+++ controllers/interfaces/types.ts
@@ -1,42 +1,29 @@
 import { Request, Response } from "express";
+import { SingleEvent } from "../../db/data/test-data/events";
 
-export interface CustomResponse extends Response {
-    status: any;
-    send: any;
-    body: any;
-    endpoints: object;
-    events: Array<object>;
-    event: object;
-    msg: string;
-    tags: Array<object>;
-    user: object;
-    userEvent: object;
-    venues: Array<object>;
-    venue: object;
-    eventTag: object;
+export interface HostEvent extends SingleEvent {
+    host: string;
 }
 
-export interface IndividualEvent {
-    event_id: number;
-    title: string;
-    slug: string;
-    event_overview: string;
-    description: string;
-    start_time: string;
-    end_time: string;
-    timezone: string;
-    venue_id: number;
-    is_online: boolean;
-    host_id: number;
-    event_type: string;
-    capacity: number;
-    attendees_count: number;
-    is_free: boolean;
-    price: number;
-    event_image_url: string;
-    is_published: boolean;
-    created_at: string;
-    last_updated_at: string;
+export interface EventAndHostAndVenue extends SingleEvent {
+    venue_name: string;
+    venue_type: string;
+    location: string;
+    venue_capacity: number;
+    facilities: string[];
+    contact_email: string;
+    contact_phone: string;
+    website_url: string;
+    event_types: string[];
+    accessibility_features: string[];
+    parking_info: string;
+    image_gallery: string[];
+    nearby_transport: string;
+    venue_created_at: string;
+    venue_last_updated_at: string;
+    name: string;
+    email: string;
+    bio: string;
 }
 
 export interface CustomError extends Error {
